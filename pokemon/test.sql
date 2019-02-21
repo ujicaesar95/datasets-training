37acb0e760f8356b3bdcb52c46aff0cd3fd13ec1
ujicaesar <fcruji@gmail.com>
2019-02-21 15:23:10 +0700

    datasets

---
+++ pokemon/test.sql
@@ -0,0 +1,16 @@
+USE toko;
+
+#create table tes (NoID INT);
+#describe tes;
+
+create table karyawan (
+No int not null auto_increment,
+Nama varchar(30) not null,
+Usia smallint,
+Berat float(3,1),
+Kota text,
+Th Year,
+Data_In timestamp,
+primary key (No));
+
+categorycategorydataset
